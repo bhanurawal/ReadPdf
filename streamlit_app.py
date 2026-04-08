bb52d7609c0f307ebbf9fc9185efec17b6af2a85
bhanurawal <81429396+bhanurawal@users.noreply.github.com>
2026-04-08 21:32:52 +0530

    Replace RetrievalQA import with new chain imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,9 @@ from sentence_transformers import SentenceTransformer, util
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-from langchain_community.chains import RetrievalQA
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
 from langchain.schema import Document
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
