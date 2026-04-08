b01b6a574bc44af635b4dbb823b6a870cb0777e9
bhanurawal <81429396+bhanurawal@users.noreply.github.com>
2026-04-08 22:13:52 +0530

    Remove unused import for RetrievalQA

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain.chains import RetrievalQA
 
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.output_parser import StrOutputParser
